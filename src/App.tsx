c5e2653fd86d2ca199b525fc7e4a8dba54e6cd3e
Jose Antonio Bravo Isidro <ja.bravo.isidro@gmail.com>
2018-10-01 16:25:15 +0100

    Block cells

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
 import './App.css';
+import Cell from './classes/cell';
 
 class App extends React.Component<any, any> {
   public state = {
     mousePosition: { x: 0, y: 0 },
   }
+
   private canvas: HTMLCanvasElement;
   private cContext: CanvasRenderingContext2D;
   private canvasWidth = 1200;
   private canvasHeight = 800;
+  private tileSize = 25;
+  private cells = Array<Array<Cell>>();
 
   public componentDidMount() {
-    this.canvas.addEventListener('mousemove', (event: MouseEvent) => {
-      const rect = this.canvas.getBoundingClientRect();
-      this.setState({ mousePosition: { x: event.clientX - rect.left, y: event.clientY - rect.top } })
-    }, false);
-
+    this.setCells();
     this.renderGrid();
   }
 
@@ -23,7 +23,7 @@ class App extends React.Component<any, any> {
     const { mousePosition } = this.state;
     return (
       <div className="App">
-        <canvas ref={ref => this.setContext(ref!)} height={`${this.canvasHeight}px`} width={`${this.canvasWidth}px`}/>
+        <canvas ref={ref => this.setContext(ref!)} height={`${this.canvasHeight}px`} width={`${this.canvasWidth}px`} onMouseMove={e => this.handleMouse(e.nativeEvent)} onMouseDownCapture={e => this.onClick(e.nativeEvent)} />
         <div className="cursor">{mousePosition.x} | {mousePosition.y}</div>
       </div>
     );
@@ -37,24 +37,78 @@ class App extends React.Component<any, any> {
   }
 
   private renderGrid() {
-    const p = 0;
-    const tileSize = 10;
+    this.cContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    const gutter = 0.5;
+
     this.cContext.lineWidth = 1;
 
-    for (var x = 0; x <= this.canvasWidth; x += tileSize) {
-      this.cContext.moveTo(0.5 + x + p, p);
-      this.cContext.lineTo(0.5 + x + p, this.canvasHeight + p);
-    }
-
-
-    for (var x = 0; x <= this.canvasHeight; x += tileSize) {
-      this.cContext.moveTo(p, 0.5 + x + p);
-      this.cContext.lineTo(this.canvasWidth + p, 0.5 + x + p);
+    for (var y = 0; y < this.canvasHeight; y += this.tileSize) {
+      for (var x = 0; x < this.canvasWidth; x += this.tileSize) {
+        this.cContext.moveTo(x + gutter, y + gutter);
+        this.cContext.lineTo(this.tileSize + x + gutter, y + gutter);
+        this.cContext.lineTo(this.tileSize + x + gutter, y + this.tileSize + gutter);
+      }
     }
 
     this.cContext.strokeStyle = "rgba(0,0,0,0.3)";
     this.cContext.stroke();
 
+    this.cells.forEach(row => {
+      row.forEach(c => {
+        this.paintCell(c);
+      })
+    })
+    requestAnimationFrame(this.renderGrid.bind(this));
+  }
+
+  private setCells() {
+    var column = 0;
+    var row = 0;
+    const cells: Cell[][] = Array<Array<Cell>>();
+    for (var y = 0; y < this.canvasHeight; y += this.tileSize) {
+      cells[row] = [];
+      for (var x = 0; x < this.canvasWidth; x += this.tileSize) {
+        cells[row][column] = new Cell(row, column, x, y);
+        column++;
+      }
+      row++;
+      column = 0;
+    }
+    this.cells = cells;
+  }
+
+  private onClick(event: MouseEvent) {
+    const canvasPosition = this.canvas.getBoundingClientRect();
+    const canvasX = Math.round(canvasPosition.left);
+    const canvasY = Math.round(canvasPosition.top);
+
+    const xInGrid = Math.trunc((event.pageX - canvasX) / this.tileSize);
+    const yInGrid = Math.trunc((event.pageY - canvasY) / this.tileSize);
+
+    this.cells[yInGrid][xInGrid].changeState();
+  }
+
+  private handleMouse(event: MouseEvent) {
+    const rect = this.canvas.getBoundingClientRect();
+    this.setState({ mousePosition: { x: event.clientX - rect.left, y: event.clientY - rect.top } });
+  }
+
+  private paintCell(cell: Cell) {
+    if (cell.state === 'blocked') {
+      const screen = this.cContext;
+      screen.beginPath();
+      screen.rect(cell.x, cell.y, this.tileSize, this.tileSize);
+      screen.fillStyle = '#DD5734';
+      screen.fill();
+      screen.closePath();
+
+      screen.beginPath();
+      screen.moveTo(cell.x, cell.y);
+      screen.rect(cell.x, cell.y, this.tileSize, this.tileSize);
+      screen.strokeStyle = "black";
+      screen.stroke();
+      screen.closePath();
+    }
   }
 }
 
